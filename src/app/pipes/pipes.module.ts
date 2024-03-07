ba7beafb5a5dfaef3a223c0d7376a0a4750a033a
Christian Martins <cmts2010@gmail.com>
2024-03-07 19:35:15 -0300

    feat: add pipe status

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
   declarations: [
     PhonePipe,
     AddressPipe,
+    StatusPipe,
   ],
   exports: [
     PhonePipe,
